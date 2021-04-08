a389d3ed318798d3a4653a007cf1a42110e5586b
David Richards <35054853+ravenOSS@users.noreply.github.com>
2021-04-07 21:55:49 -0600

    Add social component

---
+++ components/Social.js
@@ -0,0 +1,12 @@
+import Styles from './Social.module.css'
+
+export default function Social() {
+	return (
+		<div class='social'>
+			<img src='../assets/iconmonstr-github-1.svg' alt='github link' />
+			<img src='../assets/iconmonstr-linkedin-4.svg' alt='linkedin link' />
+			<img src='../assets/iconmonstr-twitter-4.svg' alt='twitter link' />
+			<img src='../assets/iconmonstr-facebook-4.svg' alt='facebook link' />
+		</div>
+	)
+}
